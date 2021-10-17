5247653475f42ace66bab228d426ef3daf37884e
vishnu <sreevishnu01@gmail.com>
2021-10-17 19:30:10 +0400

    Sidebar done & Blogpost heading moved to indside row

--- src/components/Blogpost.jsx
+++ src/components/Blogpost.jsx
@@ -19,12 +19,14 @@ function Blogpost() {
         <section className="pt-5 pb-5">
 
             <Container>
-                <div class="mb-4">
-                    <h2 class="m-0"><i class="bi bi-hourglass-top me-2"></i>Today's top highlights</h2>
-                    <p>Latest breaking news, pictures, videos, and special reports</p>
-                </div>
+
                 <Row >
+
                     <Col lg={9}>
+                        <div class="mb-4">
+                            <h2 class="m-0"><i class="bi bi-hourglass-top me-2"></i>Today's top highlights</h2>
+                            <p>Latest breaking news, pictures, videos, and special reports</p>
+                        </div>
                         {/* row1 */}
                         <Row>
                             {/* blog1 */}
@@ -55,36 +57,7 @@ function Blogpost() {
                             </Col>
 
                         </Row>
-                        {/* Row 2  */}
-                        <Row>
-                            {/* blog1 */}
-                            <Col className="mt-5">
-                                <Card className="border-light col-sm-12" >
-                                    <Card.Img src={randomImg.image} style={rounded5} />
-                                    <div className="px-0 pt-3 pb-3">
-                                        <Card.Title>12 worst types of business accounts you follow on Twitter</Card.Title>
-                                        <Card.Body>
-                                            For who thoroughly her boy estimating conviction. Removed demands expense account in outward tedious do. Particular way thoroughly unaffected
-                                        </Card.Body>
-                                    </div>
 
-                                </Card>
-                            </Col>
-                            {/* blog2 */}
-                            <Col className="mt-5">
-                                <Card className="border-light col-sm-12" >
-                                    <Card.Img src={randomImg.image} style={rounded5} className="card-img" />
-                                    <div className="px-0 pt-3 pb-3">
-                                        <Card.Title>Bad habits that people in the industry need to quit</Card.Title>
-                                        <Card.Body>
-                                            For who thoroughly her boy estimating conviction. Removed demands expense account in outward tedious do. Particular way thoroughly unaffected
-                                        </Card.Body>
-                                    </div>
-
-                                </Card>
-                            </Col>
-
-                        </Row>
                         {/* Learn more last section */}
                         <Row>
                             <Col className="col-12 text-center mt-5">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,51 @@ import { Container, Row, Col, Card } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+const randomImg = {
+    imagel: 'https://picsum.photos/1000/750',
+    images: 'https://picsum.photos/100',
+
+}
+
 function Sidebar() {
     return (
-        <Card>
-            <Card.Title>blog post 2</Card.Title>
+        <Container>
+            <h4 class="mb-3">Trending topics</h4>
+            <Card className="mb-3 text-center">
+                <Card.Title>Top 10</Card.Title>
+            </Card>
+            <Card className="mb-3 text-center">
+                <Card.Title>Bussinus</Card.Title>
+            </Card>
+            <Card className="mb-3 text-center">
+                <Card.Title>Traveling</Card.Title>
+            </Card>
+            <Card className="mb-3 text-center">
+                <Card.Title>Marketing</Card.Title>
+            </Card>
+            <div class="text-center mt-3">
+                <a href="#" class="fw-bold text-body text-primary-hover"><u>View all categories</u></a>
+            </div>
 
-        </Card>
+            <h4 class="mt-4 mb-3">Recent Post</h4>
+
+            <Row>
+                <Col sm={4}>
+                    <Card>
+                        <Card.Img src={randomImg.images} />
+                    </Card>
+
+                </Col>
+                <Col sm={8}>
+                    <Card className="border-light">
+                        <Card.Title>
+                            <h6 className="btn-link stretched-link">The pros and cons of business agency</h6>
+                        </Card.Title>
+                        <div class="small mt-1">May 17, 2021</div>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
