29369040b2bebfe7ecac044347505ec224097ff2
vishnu <sreevishnu01@gmail.com>
2021-10-29 22:11:42 +0400

    comment add done but live update not working

--- src/components/Blog/Comments.jsx
+++ src/components/Blog/Comments.jsx
@@ -1,28 +1,29 @@
 import { useState } from 'react'
 import { Card, Form, Button } from 'react-bootstrap'
 import axios from 'axios'
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function Comments({ postcomments, path }) {
 
-    const [rating, setRating] = useState("")
+    const [rating, setRateing] = useState("")
     const [comment, setComment] = useState("")
-    const token = useSelector(state => state.user.token)
+    const user = useSelector(state => state.user.token)
 
     const handelComment = async (e) => {
         e.preventDefault();
         try {
             const res = await axios.post('/comments', {
-                rating, comment, postid: path
+                rating: rating,
+                comment: comment,
+                postid: path
             }, {
-                headers: { Authorization: `Bearer ${token.token}` }
+                headers: { Authorization: `Bearer ${user.token}` }
             });
-            console.log(res)
 
-        } catch (error) {
-            console.log(error)
+        } catch (err) {
+            console.log("hellow")
         }
-    };
+    }
 
     return (
         <div>
@@ -48,18 +49,21 @@ function Comments({ postcomments, path }) {
             </Card>
             <Card>
 
-                <Form className="mt-5">
-                    <Form.Select className="me-sm-2" id="inlineFormCustomSelect" onChange={e => setRating(e.target.value)}>
-                        <option value="0">Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </Form.Select>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form onSubmit={handelComment}>
+                    <Form.Group className="mt-5 mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Comment</Form.Label>
+                        <Form.Select aria-label="Default select example" onChange={e => setRateing(e.target.value)}>
+                            <option>Open this select menu</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </Form.Select >
                         <Form.Control as="textarea" rows={3} onChange={e => setComment(e.target.value)} />
                     </Form.Group>
-                    <Button variant="primary" type="submit" onclick={handelComment} >
-                        post
+                    <Button variant="primary" type="submit">
+                        Submit
                     </Button>
                 </Form>
             </Card>
@@ -72,25 +76,3 @@ function Comments({ postcomments, path }) {
 }
 
 export default Comments
-
-
-
-
-{/* <div>
-
-<div>
-
-
-    {Object.keys(comments).map((keyName) => (
-        <div>
-            <p >{comments[keyName].author}</p>
-            <p >{comments[keyName].rating}</p>
-            <p >{comments[keyName].comment}</p>
-
-
-
-        </div>
-    ))}
-</div>
-
-</div> */}
